3c674527623e82abd9a30a289154d6c681799fd1
Connor Walsh <cwalsh24@ut.utm.edu>
2023-03-27 16:33:56 -0500

    Got some rough code written up. Need to pin down exactly what we want to do.

--- CheckersProject/src/ThemeFactory.cs
+++ CheckersProject/src/ThemeFactory.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace CheckersProject.src
 {
-    internal class ThemeFactory
+    /**internal class ThemeFactory
     {
+    }**/
+    //int colorValue = 0;
+
+    public interface ISquare
+    {
+        Board changeColor();
+    }
+
+    public interface SquareFactory
+    {
+        ISquare GetSquare();
+    }
+
+    public class Factory : SquareFactory
+    {
+        public ISquare GetSquare()
+        {
+            ISquare Red = new Red();
+            return Red;
+        }
+    }
+
+    public class Red : ISquare
+    {
+        public Board changeColor()
+        {
+            Board b = new Board();
+            b.Background = new SolidColorBrush(Colors.Red);
+            return b;
+        }
     }
 }
